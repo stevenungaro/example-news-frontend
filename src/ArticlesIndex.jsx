7a39a0c68e4bf3776675fb97fca08cb8e7cebd2f
stevenungaro <144843553+stevenungaro@users.noreply.github.com>
2023-12-06 12:56:52 -0600

    initial commit

---
+++ src/ArticlesIndex.jsx
@@ -0,0 +1,15 @@
+export function ArticlesIndex(props) {
+  return (
+    <div>
+      <h1>Hey check out these articles!</h1>
+      {props.articles.map((article) => (
+        <div key={article.title}>
+          <img src={article.urlToImage} />
+          <h2>{article.title}</h2>
+          <h3>{article.author}</h3>
+          <p>{article.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/Content.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { ArticlesIndex } from "./ArticlesIndex";
+
+export function Content() {
+  const [articles, setArticles] = useState([]);
+
+  const handleIndexArticles = () => {
+    console.log("handleIndexArticles");
+    axios.get("https://newsapi.org/v2/everything?q=tesla&apiKey=XXXX").then((response) => {
+      console.log(response.data);
+      setArticles(response.data.articles);
+    });
+  };
+
+  useEffect(handleIndexArticles, []);
+
+  return (
+    <div>
+      <main>
+        <h1>Steven Page Neato</h1>
+        <ArticlesIndex articles={articles} />
+      </main>
+    </div>
+  );
+}
